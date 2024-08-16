c6a0ec2e00e74a9bca88d02685a184789e0d14fb
Dhruv Mehta <flashxx001@gmail.com>
2024-08-17 00:39:27 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        josefin: ["Josefin Sans", "sans-serif"],
+        ballu: ["Baloo Bhai 2", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
